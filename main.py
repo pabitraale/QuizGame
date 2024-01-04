48200e239d59f5f733d3fa4994abdfec0a9d3f96
pabitraale <pabitraale25@yahoo.com>
2024-01-04 17:17:06 -0500

    added data and question model file

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
+from Question_model import Question
+from data import question_data
 
+question_bank = []
+for question in question_data:
+    question_text = question["text"]
+    question_ans = question["answer"]
+    new_question = Question(question_text, question_ans)
+    question_bank.append(new_question)
+
+
+print("question bank questions")
+print(question_bank[0].text, question_bank[0].answer)
