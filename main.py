e6fcad68dce3aeb935944acac27132dcadf91603
pabitraale <pabitraale25@yahoo.com>
2024-01-04 19:33:05 -0500

    added more function into the script

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ from quiz_brain import QuizBrain
 
 question_bank = []
 for question in question_data:
-    question_text = question["text"]
-    question_ans = question["answer"]
+    question_text = question["question"]
+    question_ans = question["correct_answer"]
     new_question = Question(question_text, question_ans)
     question_bank.append(new_question)
 
 
-print("question bank questions")
-print(question_bank[0].text, question_bank[0].answer)
-
 quiz = QuizBrain(question_bank)
-quiz.next_question()
+
+
+while quiz.still_has_question():
+    quiz.next_question()
+print("You've completed the quiz.")
+print(f"Your final score is: {quiz.score}/{quiz.question_number}")
+
